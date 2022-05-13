a8242a5eac7bc8e95be4113322618495295bcf4c
John Doe <leehy0782@naver.com>
2022-05-14 01:36:25 +0900

    feat: Get all product to Mainpage

---
+++ src/components/Mainpage/Products.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const Cont = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 30.625rem;
+`
+
+const Img = styled.img`
+    width: 23.75rem;
+    height: 23.75rem;
+    border-radius: 10px;
+    background-color: aqua;
+    border: 1px solid #C4C4C4;
+    margin-bottom: 0.375rem;
+`
+
+const Seller = styled.p`
+    font-size: ${({ fontSize }) => fontSize};
+    font-weight: ${({ fontWeight }) => fontWeight};
+    color: ${({ color }) => color};
+`
+
+const Title = styled(Seller)``
+const Price = styled(Seller)``
+
+function Products() {
+    const url = `https://openmarket.weniv.co.kr/products/`;
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        axios.get(url)
+        .then(function (response) {
+            const productData = response.data.results;
+            setProduct(productData);
+          })
+          .catch(function (error) {
+            // 에러 핸들링
+            console.log(error);
+          })
+          .then(function () {
+            // 항상 실행되는 영역
+          });
+    }, [])
+
+    return(
+        <>
+            {product.map((product) => (
+            <Cont key={product.product_id}>
+                <Img src={product.image}></Img>
+                <Seller fontSize={'1rem'} fontWeight={'400'} color={'#767676'}>{product.seller_store}</Seller>
+                <Title fontSize={'1.125rem'} fontWeight={'400'} color={'#000000'}>{product.product_name}</Title>
+                <Price fontSize={'1.5rem'} fontWeight={'700'} color={'#000000'}>{product.price}</Price>
+            </Cont>
+            ))}
+        </>
+    )
+}
+
+export default Products;
--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Slider from "../components/Mainpage/Slider";
+import Products from "../components/Mainpage/Products";
 
 function Mainpage() {
     return(
         <>
             <Header />
             <Slider />
+            <Products />
         </>
     )
 }
