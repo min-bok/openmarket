bcce6eb3c29d9b32ddfdb1049f20cc611cb96457
John Doe <leehy0782@naver.com>
2022-05-14 01:16:25 +0900

    feat: Add Slider

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,8 +1,12 @@
 import Header from "../components/Header";
+import Slider from "../components/Mainpage/Slider";
 
 function Mainpage() {
     return(
-        <Header />
+        <>
+            <Header />
+            <Slider />
+        </>
     )
 }
 
